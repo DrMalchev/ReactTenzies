5959f886e4825f53182f69cf29d014725855756e
DrMalchev <dr.delyan.malchev@gmail.com>
2023-10-23 10:40:14 +0300

    WIP

--- reacttenzies/src/App.js
+++ reacttenzies/src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import Die from './components/Die'
 
 export default function App() {
+
+    const [numbers, setNumbers] = React.useState(allNewDice())
+
+    function allNewDice() {
+        const array = Array.from(Array(10), () => Math.floor(Math.random() * 6))
+
+        return array.map(
+            (x, i) => ({ value: x, isHeld: false, id: i })
+        )
+    }
+
+    const dies = numbers.map((x) => {
+        return <Die value={x.value} key={x.id} />
+    })
+
+    function rollDice() {
+        setNumbers(allNewDice())
+    }
+
     return (
         <main>
-            <div className='innerDiv'>Test</div>
-            
+            <div className='innerDiv'>
+                <div className='dieContainer'>
+                    {dies}
+                </div>
+                <div>
+                    <button className="rollButton" onClick={rollDice}>Roll</button>
+                </div>
+            </div>
+
         </main>
     )
 }
